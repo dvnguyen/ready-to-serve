a56fd4a2ea43a1616c0dfb180a9e6d9e617dc2f4
David Nguyen <dnguyen@Mac-Mini.local>
2016-06-25 16:37:34 -0500

    Add landing page functionality

---
+++ src/app/app.module.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+  	.module('app', [
+  		// Angular modules
+  		'ngRoute',
+
+  		// Third-party modules
+  		'firebase',
+
+  		// Custom modules
+  		'app.landing'
+  	]);
+})();
