873742dfc82287e62e8479552a382e914d8ff774
David Nguyen <dvnguyen@gmail.com>
2016-06-29 20:38:02 -0500

    Create the authService

---
+++ src/app/auth/auth.service.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .factory('authService', authService);
+
+  authService.$inject = [];
+
+  function authService() {
+    var service = {
+
+    };
+
+    return service;
+
+    ///////////////
+
+
+  }
+
+})();
