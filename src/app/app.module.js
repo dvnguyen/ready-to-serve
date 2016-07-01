83ea0d75045e4c055b90179341b882ad3d8686f7
David Nguyen <dvnguyen@gmail.com>
2016-07-01 14:19:21 -0500

    Use .otherwise to catch unknown URLs

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -15,5 +15,14 @@
   		'app.landing',
   		'app.layout',
   		'app.waitList'
-  	]);
+  	])
+  	.config(configFunction);
+
+  configFunction.$inject = ['$routeProvider'];
+
+  function configFunction($routeProvider) {
+  	$routeProvider.otherwise({
+  		redirectTo: '/'
+  	});
+  }
 })();
